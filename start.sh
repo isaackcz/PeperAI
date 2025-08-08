7ce7a03273e2e541e0890161a02dbc14b238dbbd
isaackcz <isaackcz@gmail.com>
2025-08-08 12:01:29 +0800

    Add workers flag to uvicorn for Railway deployment

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 # Railway startup script
 # Use Railway's PORT if available, otherwise default to 8000
 PORT=${PORT:-8000}
-exec uvicorn main:app --host 0.0.0.0 --port $PORT
+exec uvicorn main:app --host 0.0.0.0 --port $PORT --workers 1
